c79065fd5a3a3ba91df7e52a58f5789b24974003
Sheraz Tariq <sheraztariq@hotmail.com>
2017-05-29 05:52:01 -0400

    got api working

--- quickstart.py
+++ quickstart.py
@@ -4,11 +4,6 @@ from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 
-
-# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
-# tab of
-#   https://cloud.google.com/console
-# Please ensure that you have enabled the YouTube Data API for your project.
 DEVELOPER_KEY = "AIzaSyA0W-NMZ7LnwIxCuwj3vHLI2xOg8lKg-d8"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
@@ -26,11 +21,9 @@ def youtube_search(options):
   ).execute()
 
   videos = []
-  channels = []
-  playlists = []
-
 
   for search_result in search_response.get("items", []):
+    # pdb.set_trace()
     if search_result["id"]["kind"] == "youtube#video":
       videos.append("%s (%s)" % (search_result["snippet"]["title"],
                                  search_result["id"]["videoId"]))
@@ -38,7 +31,7 @@ def youtube_search(options):
   print "Videos:\n", "\n".join(videos), "\n"
 
 if __name__ == "__main__":
-  argparser.add_argument("--q", help="Search term", default="Google")
+  argparser.add_argument("--q", help="Search term", default="Funny Song")
   argparser.add_argument("--max-results", help="Max results", default=10)
   args = argparser.parse_args()
 
